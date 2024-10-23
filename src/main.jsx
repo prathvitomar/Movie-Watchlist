d65dccf91a4d084a8767b5156c1fbb3b66cea910
Prathvi Singh Tomar <108338096+prathvitomar@users.noreply.github.com>
2024-10-23 15:49:59 +0530

    Update main.jsx for resolving Github Pages Issue

--- src/main.jsx
+++ src/main.jsx
@@ -49,7 +49,8 @@ const router = createBrowserRouter([
   {
     path: '/login',
     element: <Login />,
-  }
+  },
+  { basename: '/Movie-Watchlist' }
 ]);
 
 createRoot(document.getElementById('root')).render(
